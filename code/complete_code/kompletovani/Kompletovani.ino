c9f2d16641e2a9953361d5ea8ffbe3b53f4c1478
MadChipmunk03 <52546468+MadChipmunk03@users.noreply.github.com>
2021-08-20 20:43:08 +0200

    Add files via upload

---
+++ code/complete_code/kompletovani/Kompletovani.ino
@@ -0,0 +1,61 @@
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <Wire.h>
+
+/*#include <SPI.h>
+#include <SD.h>
+File myFile;*/
+
+const byte DNS_PORT = 53;
+IPAddress apIP(172, 217, 28, 1);
+DNSServer dnsServer;
+ESP8266WebServer server(80);
+
+#include "website.h"
+//#include "MacroEngine.h"
+#include "handleVyber.h"
+
+void handleNotFound() {
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
+
+void setup() {
+
+  Serial.begin(115200);
+  Wire.begin();             //ATtiny85 is slave #10
+  delay(250);
+
+  /*if (!SD.begin(9)) {
+    Serial.println("SD card initialization failed!");
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    return;
+  }
+  else {
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);
+  }*/
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP("WiFiUSB", "");
+
+  // if DNSServer is started with "*" for domain name, it will reply with
+  // provided IP to all DNS request
+  dnsServer.start(DNS_PORT, "usb.com", apIP);
+
+  server.on("/", HTTP_GET, handleRoot);        // Call the 'handleRoot' function when a client requests URI "/"
+  server.on("/vyber", HTTP_POST, handleVyber);
+  server.onNotFound(handleNotFound);           // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+
+  server.begin();
+}
+
+void loop() {
+  dnsServer.processNextRequest();
+  server.handleClient();
+}
---
+++ code/complete_code/kompletovani/handleVyber.h
@@ -0,0 +1,101 @@
+/*
+   handy websites
+
+   ATtiny85:
+   https://www.usb.org/sites/default/files/documents/hut1_12v2.pdf (CHAPTER 10)
+   https://github.com/digistump/DigistumpArduino/blob/master/digistump-avr/libraries/DigisparkKeyboard/DigiKeyboard.h
+
+   ESP8266:
+   https://tttapa.github.io/ESP8266/Chap10%20-%20Simple%20Web%20Server.html
+   https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+*/
+
+byte POSTargToByte(String message) {
+  byte content[2];
+  for (int i = 0; i < message.length(); i++) {
+    content[i] = byte(message[i]) - 48;
+    if (i != 0) {
+      content[0] = content[0] * 10 + content[i];
+    }
+  }
+  return content[0];
+}
+
+void handleVyber() {
+
+  //searching for the defined POST arg.
+  if (server.hasArg("SingleKeystroke")) {
+    //server.send(200, "text/html", "<h1>You have hit a keystroke</h1><p>your keystoke was: " + server.arg("SingleKeystroke") + "</p>");
+    server.sendHeader("Location", "/");
+    server.send(303);
+
+    byte keyStrokeByte = POSTargToByte(server.arg("SingleKeystroke"));
+
+    //transmitting keystroke
+    Wire.beginTransmission(10);  // transmit to ATtiny85 (slave #10)
+    Wire.write("k");             // define "k" - single Keystroke
+    Wire.write(keyStrokeByte);
+    Wire.endTransmission();      // stop transmitting
+  }
+
+  else if (server.hasArg("InputBoxPrintln")) {
+    //server.send(200, "text/html", "<h1>You have written</h1><p>" + server.arg("InputBoxPrintln") + "</p>");
+    server.sendHeader("Location", "/");
+    server.send(303);
+
+    char buffer[128];
+    server.arg("InputBoxPrintln").toCharArray(buffer, 128);
+
+    //transmitting text
+    Wire.beginTransmission(10);  // transmit to ATtiny85 (slave #10)
+    Wire.write("p");             // define "p" - print text
+    Wire.write(buffer);
+    Wire.endTransmission();      // stop transmitting
+  }
+
+  else if (server.hasArg("MultipleKeystrokes1")) {
+
+    server.sendHeader("Location", "/");
+    server.send(303);
+
+    byte keyStrokeByte1 = POSTargToByte(server.arg("MultipleKeystrokes1"));
+    byte keyStrokeByte2 = POSTargToByte(server.arg("MultipleKeystrokes2"));
+    byte keyStrokeByte3 = POSTargToByte(server.arg("MultipleKeystrokes3"));
+    byte modKeyByte1 = POSTargToByte(server.arg("ModKeys1"));
+    byte modKeyByte2 = POSTargToByte(server.arg("ModKeys2"));
+    byte modKeyByte3 = POSTargToByte(server.arg("ModKeys3"));
+
+    if (keyStrokeByte1 == 0) {
+      if (keyStrokeByte2 != 0) {
+        keyStrokeByte1 = keyStrokeByte2;
+        keyStrokeByte2 = 0;
+      }
+      else if (keyStrokeByte3 != 0) {
+        keyStrokeByte1 = keyStrokeByte3;
+        keyStrokeByte3 = 0;
+      }
+    }
+
+    //transmitting keystroke
+    Wire.beginTransmission(10);  // transmit to ATtiny85 (slave #10)
+    Wire.write("m");             // define "m" - multiple Keystrokes
+    Wire.write(keyStrokeByte1);
+    Wire.write(keyStrokeByte2);
+    Wire.write(keyStrokeByte3);
+    Wire.write(modKeyByte1);
+    Wire.write(modKeyByte2);
+    Wire.write(modKeyByte3);
+    Wire.endTransmission();      // stop transmitting
+  }
+  /*else if (server.hasArg("button")) {
+    server.sendHeader("Location", "/");
+    server.send(303);
+    Serial.println("handleVyber");
+    MacroEngine();
+  }*/
+
+  //if POST arg. isn't defined - send it
+  else {
+    server.send(400, "text/html", "<h1>Unknown post</h1><p>POST body: " + server.arg("plain") + "</p>");
+  }
+}
