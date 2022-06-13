4a0424c3f87f0befbd63c3ada7cf0447ba506456
MadChipmunk03 <52546468+MadChipmunk03@users.noreply.github.com>
2022-06-13 12:25:16 +0200

    working example

--- MultiUSB_API/src/main.cpp
+++ MultiUSB_API/src/main.cpp
@@ -1,9 +1,112 @@
-#include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
+/*
+ *  Json parametric GET REST response
+  *  by Mischianti Renzo <https://www.mischianti.org>
+ *
+ *  https://www.mischianti.org/
+ *
+ */
+ 
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+ 
+const char* ssid = "Mobile-A51";
+const char* password = "123456Abc";
+ 
+ESP8266WebServer server(80);
+ 
+// Serving Hello world
+void getHelloWord() {
+    server.send(200, "text/json", "{\"name\": \"Hello world\"}");
+}
+// Serving Hello world
+void getSettings() {
+    String response = "{";
+ 
+    response+= "\"ip\": \""+WiFi.localIP().toString()+"\"";
+    response+= ",\"gw\": \""+WiFi.gatewayIP().toString()+"\"";
+    response+= ",\"nm\": \""+WiFi.subnetMask().toString()+"\"";
+ 
+    if (server.arg("signalStrength")== "true"){
+        response+= ",\"signalStrengh\": \""+String(WiFi.RSSI())+"\"";
+    }
+ 
+    if (server.arg("chipInfo")== "true"){
+        response+= ",\"chipId\": \""+String(ESP.getChipId())+"\"";
+        response+= ",\"flashChipId\": \""+String(ESP.getFlashChipId())+"\"";
+        response+= ",\"flashChipSize\": \""+String(ESP.getFlashChipSize())+"\"";
+        response+= ",\"flashChipRealSize\": \""+String(ESP.getFlashChipRealSize())+"\"";
+    }
+    if (server.arg("freeHeap")== "true"){
+        response+= ",\"freeHeap\": \""+String(ESP.getFreeHeap())+"\"";
+    }
+    response+="}";
+ 
+    server.send(200, "text/json", response);
+}
+ 
+// Define routing
+void restServerRouting() {
+    server.on("/", HTTP_GET, []() {
+        server.send(200, F("text/html"),
+            F("Welcome to the REST Web Server"));
+    });
+    server.on(F("/helloWorld"), HTTP_GET, getHelloWord);
+    server.on(F("/settings"), HTTP_GET, getSettings);
+}
+ 
+// Manage not found URL
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup(void) {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+ 
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+ 
+  // Activate mDNS this is used to be able to connect to the server
+  // with local DNS hostmane esp8266.local
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+    // Serial.println();
+  }
+ 
+  // Set server routing
+  restServerRouting();
+  // Set not found response
+  server.onNotFound(handleNotFound);
+  // Start server
+  server.begin();
+  Serial.println("HTTP server started");
+}
+ 
+void loop(void) {
+  server.handleClient();
+  MDNS.update();
 }
