8251420494bec3edb7ff3bf50d45ef55a787ff3e
MadChipmunk03 <petaslachta@gmail.com>
2022-06-20 20:45:27 +0200

    dark theme support - commented out for now

--- MultiUSB_UI/src/App.vue
+++ MultiUSB_UI/src/App.vue
@@ -5,7 +5,7 @@
 
     <v-main>
       <!-- <v-btn @click="sendTestGET">Send GET on esp8266/helloWorld</v-btn> -->
-		<router-view></router-view>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -21,14 +21,23 @@ export default {
   components: {
     NavigationDrawer,
     AppBar,
-    
-},
+  },
   methods: {
     async sendTestGET() {
       const res = await axios.get("/api/helloWorld");
       console.log("💌", res);
     },
   },
+  // beforeMount() {
+  //   //check if browser supports dark mode.
+  //   this.$vuetify.theme.isDark = false;
+  //   if (window.matchMedia && window.matchMedia("(prefers-color-scheme)").media !== "not all") {
+  //     //if user prefers light mode switch to light mode
+  //     if (!window.matchMedia("(prefers-color-scheme: light)").matches) {
+  //       this.$vuetify.theme.isDark = true;
+  //     }
+  //   }
+  // },
 };
 </script>
 
--- MultiUSB_UI/src/components/AppBar.vue
+++ MultiUSB_UI/src/components/AppBar.vue
@@ -9,9 +9,9 @@ export default {
 </script>
 
 <template>
-  <v-app-bar color="primary" elevation="4" height="65" app>
+  <v-app-bar light color="primary" elevation="4" height="65" app>
     <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
-    <v-toolbar-title>{{ this.$store.state.appBarTitle }}</v-toolbar-title>
+    <v-app-bar-title>{{ this.$store.state.appBarTitle }}</v-app-bar-title>
   </v-app-bar>
 </template>
 
